8c1139fd8374eefe4172d3c174861933aa700b48
Maurice <capdiz@yahoo.com>
2012-08-20 20:51:44 +0300

    Email commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ MyLmp::Application.configure do
   
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
   config.action_mailer.smtp_settings = { 
 	:address =>	"smtp.gmail.com",
