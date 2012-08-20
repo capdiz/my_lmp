8c19fa24dd559c70cf0ab1d1297ac47b7bc6974e
Maurice <capdiz@yahoo.com>
2012-08-20 18:07:56 +0300

    Enviroment commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -76,7 +76,6 @@ Devise.setup do |config|
   # this period, the invited resource won't be able to accept the invitation.
   # When invite_for is 0 (the default), the invitation won't expire.
   config.invite_for = 2.weeks
-  config.encoding = "utf-8"
   
   # Number of invitations users can send.
   # If invitation_limit is nil, users can send unlimited invitations.
