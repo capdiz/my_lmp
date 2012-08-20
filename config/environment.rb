39c6f0caaa4751b334b7b88225509fffd6876e3f
Maurice <capdiz@yahoo.com>
2012-08-20 08:50:37 +0300

    Edit template

--- config/environment.rb
+++ config/environment.rb
@@ -8,5 +8,6 @@ MyLmp::Application.initialize!
 	:address =>	"smtp.mandrillapp.com",
 	:port =>	587,
 	:user_name =>	ENV["capdiz@yahoo.com"],
-	:password =>	ENV["70df126e-e283-4a10-939c-75b3130376ef"]
+	:password =>	ENV["70df126e-e283-4a10-939c-75b3130376ef"],
+	:domain => "www.mylmp.us"
   }	
