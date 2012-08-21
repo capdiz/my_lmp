efbf67ab0f1cfe0d2f0b151b48222e8e5046e7d3
Maurice <capdiz@yahoo.com>
2012-08-21 19:29:15 +0300

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,9 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-namespace :db do
-	desc "Fill category table with our first sample categories"
-	task :populate => :environment do
-		Rake::Task['db:reset'].invoke
-	#	make_users
-		make_categories
-		
-	end
-end
-
-def make_categories
 	categories = ["Automobile", "Automobile Accessories", "Computer Accessories", "Bags (Backbacks, handbags and purses)", "Consumer Electronics", "Cosmetics", "Electronics", "Foods & Beverages", "Garments & Textile", "Handmade Crafts and Jewelry", "Heavy Machinery", "Housing & Furniture", "Phone accessories", "Real-estate", "Self Written & Published Books", "Self Written & Produced Music"]
 	for cats in categories do
 		def_cat = Category.create!(:category_name => cats)
 	end
-end
+
 
