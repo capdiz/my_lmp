d01795cec9d222e7b23283caccad822e08968ece
Maurice <capdiz@yahoo.com>
2012-08-21 18:10:14 +0300

    Supplier image commit

--- app/models/profile_photo.rb
+++ app/models/profile_photo.rb
@@ -28,36 +28,39 @@ class ProfilePhoto < ActiveRecord::Base
 					  
 
 	validates_attachment_size :profile_image, :less_than => 3.megabytes
-
+   
 	before_profile_image_post_process do |image|
-		if image.source_changed? && image.profile_image_changed?
-			image.processing = true
-			false
-		end
+ 		if !image.processing && image.profile_image_changed?
+       			image.processing = true
+       			false # halts processing
+ 		end	     
 	end
 
+	# call method from after_save that will be processed in the background
 	after_save do |image|
-		if image.processing
-			processImageJob(image)
-		end
+ 		if image.processing
+       			processImageJob(image)
+ 		end
 	end
 
-	def processImageJob
+	def processImageJob(image)
 		image.regenerate_styles!
 	end
 	handle_asynchronously :processImageJob
-
+	      
+	# generate styles (downloads original first)
 	def regenerate_styles!
-		self.profile_image.reprocess!
-		self.processing = false
-		self.save(:validate => false)
+ 		self.profile_image.reprocess!
+ 		self.processing = false  
+ 		self.save(:validations => false)
 	end
 
+	# detect if our source file has changed
 	def profile_image_changed?
-		self.profile_image_file_size_changed? ||
+ 		self.profile_image_file_size_changed? ||
 		self.profile_image_file_name_changed? ||
-		self.profile_image_content_type_changed? ||
-		self.profile_image_updated_at_changed?
+	  	self.profile_image_content_type_changed? ||
+  	        self.profile_image_updated_at_changed?
 	end
-
+	
 end
