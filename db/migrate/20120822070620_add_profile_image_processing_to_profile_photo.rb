22469f7d06e780100286586fc8d6af4bd9c67ff9
Maurice <capdiz@yahoo.com>
2012-09-04 12:32:13 +0300

    Deleteted migrations

--- db/migrate/20120822070620_add_profile_image_processing_to_profile_photo.rb
+++
@@ -1,9 +0,0 @@
-class AddProfileImageProcessingToProfilePhoto < ActiveRecord::Migration
-  def self.up
-	  add_column :profile_photos, :profile_image_processing, :boolean
-  end
-
-  def self.down
-	  remove_column :profile_photos, :profile_image_processing
-  end  
-end
